9b902821707fbabe4351ce35db4326ff32c4fbbf
Filipp Balakin <filipp@balakin.ru>
2026-04-12 17:43:19 +0300

    feat: register database check in AppConfig.ready()

--- django_celery_outbox/apps.py
+++ django_celery_outbox/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class DjangoCeleryOutboxConfig(AppConfig):
     name = 'django_celery_outbox'
     default_auto_field = 'django.db.models.BigAutoField'
+
+    def ready(self) -> None:
+        from django_celery_outbox import checks  # noqa: F401
